2a233438d7e4f5cc0bffcd5e90a67f7ee9b49ae9
William Ndongmo <williamndongmo88@gmail.com>
2025-07-23 06:01:45 +0200

    Ajout WhatsappConfig

--- src/main/java/will/dev/Artisan_des_saveurs/config/WhatsappConfig.java
+++ src/main/java/will/dev/Artisan_des_saveurs/config/WhatsappConfig.java
@@ -8,27 +8,5 @@ import will.dev.Artisan_des_saveurs.services.WhatsappNotification;
 
 @Configuration
 public class WhatsappConfig {
-
-    @Value("${app.whatsapp.enabled}")
-    private boolean whatsappEnabled;
-
-    @Value("${twilio.account.sid}")
-    private String accountSid;
-
-    @Value("${twilio.auth.token}")
-    private String authToken;
-
-    @Value("${twilio.whatsapp.from}")
-    private String fromNumber;
-
-    @Bean
-    public WhatsappNotification whatsappNotification() {
-        if (whatsappEnabled) {
-            Twilio.init(accountSid, authToken);
-            return new WhatsappNotification(fromNumber);
-        } else {
-            return new WhatsappNotification(null); // mode désactivé
-        }
-    }
 }
 
--- src/main/java/will/dev/Artisan_des_saveurs/services/WhatsappNotification.java
+++ src/main/java/will/dev/Artisan_des_saveurs/services/WhatsappNotification.java
@@ -3,15 +3,14 @@ package will.dev.Artisan_des_saveurs.services;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 import will.dev.Artisan_des_saveurs.entity.ContactRequest;
 import will.dev.Artisan_des_saveurs.entity.User;
 
 @Service
-@Configuration
 @RequiredArgsConstructor
 public class WhatsappNotification {
 
@@ -24,7 +23,11 @@ public class WhatsappNotification {
     @Value("${twilio.whatsapp.from}")
     private String fromPhoneNumber;
 
-    public WhatsappNotification(String fromPhoneNumber) {
+    @PostConstruct
+    public void init() {
+        System.out.println("accountSid = " + accountSid);
+        System.out.println("authToken = " + authToken);
+        System.out.println("fromPhoneNumber = " + fromPhoneNumber);
     }
 
     public void sendWhatsappMessage(User user, String to, ContactRequest contactRequest, Boolean isFromCart) {
