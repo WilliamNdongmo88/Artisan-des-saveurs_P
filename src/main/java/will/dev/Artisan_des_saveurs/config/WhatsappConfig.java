ed5097a40b0e2d8532faba2acad17c764ec695b1
William Ndongmo <williamndongmo88@gmail.com>
2025-07-23 15:44:19 +0200

    MAJ du fichier .properties

--- src/main/java/will/dev/Artisan_des_saveurs/config/WhatsappConfig.java
+++ src/main/java/will/dev/Artisan_des_saveurs/config/WhatsappConfig.java
@@ -1,12 +1,12 @@
 package will.dev.Artisan_des_saveurs.config;
 
-import com.twilio.Twilio;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import will.dev.Artisan_des_saveurs.services.WhatsappNotification;
-
-@Configuration
-public class WhatsappConfig {
-}
+//import com.twilio.Twilio;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import will.dev.Artisan_des_saveurs.services.WhatsappNotification;
+//
+//@Configuration
+//public class WhatsappConfig {
+//}
 
